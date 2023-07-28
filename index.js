8b383132551242f125f60189fecd068e8a82734b
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-07-27 20:59:57 -0400

    created new folders and files

--- index.js
+++ index.js
@@ -1,42 +1,8 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer'); 
+const generateLogo = require('./_other/old/generateLogo');
+const fs = require('fs');
 
-const confirmAnswerValidator = async (input) => {
-    if (!questions.text.length == 3) {
-       return 'Must be 3 letters long. Try again!';
-    }
-    return true;
-};
 
-questions = [
-    {
-        name: 'text',
-        type: 'input',
-        message: 'Choose three letters for your logo.',
-        default: 'SVG',
-        validate: confirmAnswerValidator,
-    },
-    {
-        name: 'textColor',
-        type: 'rawlist',
-        message: 'Which color would you like the letters to be?',
-        choices: ['Black', 'Blue', 'Red'],
-        default: '#000000',
-    },
-    {
-        name: 'shape',
-        type: 'rawlist',
-        message: 'Choose a shape to house the letters.',
-        choices: ['Circle', 'Triangle', 'Square'],
-        default: 'circle',
-    },
-    {
-        name: 'shapeColor',
-        type: 'rawlist',
-        message: 'Which color would you like the shape to be?',
-        choices: ['White', 'Gray', 'Beige'],
-        default: '#FFFFFF',
-    },
-]
 
 function writeToFile(filename, data) {
     fs.writeFile(filename, data, (err) => 
@@ -46,7 +12,9 @@ function writeToFile(filename, data) {
 function init() {
     inquirer.prompt(questions)
         .then(function (answers) {
-            writeToFile(`logo.svg`, generateLogo(answers));
+            const svg = generateLogo(answers);
+            colorPicker(answers);
+            writeToFile(`logo.svg`, svg);
         })
 }
 

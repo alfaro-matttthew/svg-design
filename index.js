629cc52ddf2fd45ad8e56565811d6b7c36af769d
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-07-30 17:43:06 -0400

    a mistake

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const inquirer = require('inquirer'); 
-const generateLogo = require('./_other/old/generateLogo');
-const fs = require('fs');
+const CLI = require("./lib/cli.js");
 
+const cli = new CLI();
 
-
-function writeToFile(filename, data) {
-    fs.writeFile(filename, data, (err) => 
-    err ? console.log(err) : console.log('Success!'))
-};
-
-function init() {
-    inquirer.prompt(questions)
-        .then(function (answers) {
-            const svg = generateLogo(answers);
-            colorPicker(answers);
-            writeToFile(`logo.svg`, svg);
-        })
-}
-
-init();
+cli.init();
